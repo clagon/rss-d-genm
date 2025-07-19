fc37ab08ad904c5e612bc3045e8e77b6aaab173b
sora_12 <karbonarask@gmail.com>
2025-07-19 13:59:22 +0000

    feat: create basic layout and header

---
+++ src/src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavLi, NavUl } from 'flowbite-svelte';
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">RSS-D-GENM</span>
+	</NavBrand>
+	<NavUl>
+		<NavLi href="/">Feeds</NavLi>
+		<NavLi href="/admin/feeds">Admin Feeds</NavLi>
+		<NavLi href="/admin/tags">Admin Tags</NavLi>
+	</NavUl>
+</Navbar>
--- src/src/routes/+layout.svelte
+++ src/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Header />
+
+<main class="p-4">
+	{@render children()}
+</main>
+
+
